3748e39b3c949b49227a97e99ba455a150ecbb21
Anton Tobias Jensen <41019970+PenguinsHaveWings@users.noreply.github.com>
2018-07-27 22:47:50 +0200

    Update Congress.java

--- Congress.java
+++ Congress.java
@@ -3,8 +3,8 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Congress3{
-  private static int n;                       // number of items on priority queue
+public class Congress {
+  private static int n; // number of items on priority queue
   private static long seats;
 
   //----------------------------------------------------------
@@ -32,35 +32,6 @@ public class Congress3{
 
     public int compareTo(State other){
       return Double.compare(this.qouta, other.qouta);
-    }
-  }
-
-  //-------------------------------------------------------------
-
-  public static void main(String[] args) {
-    n = StdIn.readInt();
-    seats =  StdIn.readLong();
-    StdIn.readLine();
-    MaxPQ<State> states = new MaxPQ<State>();
-
-    while(!StdIn.isEmpty()){
-      String name = StdIn.readLine();
-      Long population = Long.parseLong(StdIn.readLine());
-      State state = new State(name, population);
-      states.insert(state);
-      seats--;
-    }
-
-    while(seats > 0){
-      State high = states.delMax();
-      high.addSeat();
-      states.insert(high);
-      seats--;
-    }
-
-    while(!states.isEmpty()){
-      State stat = states.delMax();
-      stat.print();
     }
   }
 }
